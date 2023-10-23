5f5335b1167479adb6809fbf7b97fa44ae2a6b31
TavonnS <142205891+TavonnS@users.noreply.github.com>
2023-10-23 16:55:55 -0700

    first draft commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = require('./questions');
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(productREADME, markdownContent) {
+
+fs.writeFile('./productREADME.md', markdownContent, err => {
+    if (err) {
+        console.error('Error writing file', err)
+    } else {
+        console.log('README created at productREADME.md')
+    }
+});
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions();
+    // writeToFile();
+}
 
 // Function call to initialize app
 init();
---
+++ Develop/questions.js
@@ -0,0 +1,69 @@
+const inquirer = require('inquirer');
+
+const questions =  () => {
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your application?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter the description of your application...'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter the instructions for installing your applicaiton...'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the usage information of your application...'
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        messsage: 'What are the guidelines for contributing to this application? Describe here...'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What information is needed for testing this application? Describe here...'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application from this list:',
+        choices: ['MIT', 'Microsoft Public License	', 'Mozilla Public License', 'Eclipse Public License', 'Academic Free License', 'Creative Commons']
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email', 
+        message: 'What is your email?'
+    }
+]
+)
+.then(answers => {
+    const markdownContent = 
+    `#${answers.title}
+
+    ##Description
+    ${answers.description}
+    ##Table of Contents
+    ##Questions
+    ##Usage Instructions
+    `
+
+
+});
+};
+
+module.exports = questions;
